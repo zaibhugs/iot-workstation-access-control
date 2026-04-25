543fbe4696a89d843b6b37274a128d9b7a1a905a
Zios_Bug_os <165111148+ZaydiMucode@users.noreply.github.com>
2026-04-25 10:35:19 +0800

    Change test to check for login redirection

--- tests/Feature/ExampleTest.php
+++ tests/Feature/ExampleTest.php
@@ -1,7 +1,7 @@
 <?php
 
-test('the application returns a successful response', function () {
+test('the application redirects to login', function () {
     $response = $this->get('/');
 
-    $response->assertStatus(200);
+    $response->assertRedirect('/login'); // or ->assertStatus(302)
 });
